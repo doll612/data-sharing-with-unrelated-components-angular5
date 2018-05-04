79c31a2a0830fa949904071109968d772f98f2ed
doll612 <priyankadoll612@gmail.com>
2018-05-05 01:03:18 +0530

    sharedService

---
+++ list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../services/...';
+
+@Component({
+  selector: 'app-listComponent',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent{
+
+constructor(private sharedService: SharedService) {
+    
+  }
+  
+  ngOnInit() {
+    this.sharedService.currentMessage.subscribe(message => {
+      this.list = [];
+      this.vesselVisitList = message
+    })
+    
+    }
